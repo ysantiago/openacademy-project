7f5c7ad5c9d0417cd95145732b06da6450a337a1
Yennifer Santiago <yennifer@vauxoo.com>
2016-02-04 15:04:11 +0000

    [REF] openacademy: Adding a modify method

--- openacademy/model/openacademy_course.py
+++ openacademy/model/openacademy_course.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields
+from openerp import api, models, fields
 
 '''
 This module create model of Course
@@ -28,3 +28,18 @@ class Course(models.Model):
          'UNIQUE(name)',
          "The course title must be unique"),
     ]
+
+    @api.multi
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Course, self).copy(default)
+
