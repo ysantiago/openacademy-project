5d223a1cc9870e6edae220fda9a781af7fa594eb
Yennifer Santiago <yennifer@vauxoo.com>
2016-02-16 13:30:09 +0000

    [FIX] openacademy: Finding unitest errors

--- openacademy/wizard/openacademy_wizard.py
+++ openacademy/wizard/openacademy_wizard.py
@@ -7,12 +7,12 @@ class Wizard(models.TransientModel):
     _name = 'openacademy.wizard'
 
     def _default_sessions(self):
-        return self.env['openacademy.session'].browse(
-                    self._context.get('active_ids'))
+        return self.env[
+            'openacademy.session'].browse(self._context.get('active_ids'))
 
     session_ids = fields.Many2many('openacademy.session',
-                                string="Sessions", required=True,
-                                default=_default_sessions)
+                                   string="Sessions", required=True,
+                                   default=_default_sessions)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
     @api.multi
